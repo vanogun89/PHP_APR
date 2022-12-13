30f46db2016407988829ee7593c92c6ea31fddb6
vanogun89 <vanya.panferov@list.ru>
2022-12-13 21:40:32 +0300

    The forth update!

--- Project_APR.php
+++ Project_APR.php
@@ -15,26 +15,32 @@ function show_all($name_table, $user, $password)
     }
 }
 
-function inc_id($con, $table_name)
+function inc_id($table_name, $user, $password)
 {
-    $first_p = "SELECT id FROM $table_name";
-    $stid = oci_parse($con, $first_p);
-    oci_execute($stid);
-    $ert = array();
-    $r = oci_fetch_all($stid, $ert, OCI_ASSOC);
-    if (strtolower($table_name) == 'humans'){
-        $new_id = max($ert["ID"]) + 1;
-    } else if (strtolower($table_name) == 'orgs') {
-        $new_id = max($ert["ID"]) + 10;
+    $new_id = -1;
+    $connection = oci_connect($user, $password, 'localhost/XEPDB1');
+    if ($connection) {
+        $first_p = "SELECT id FROM $table_name";
+        $stid = oci_parse($connection, $first_p);
+        oci_execute($stid);
+        $ert = array();
+        $r = oci_fetch_all($stid, $ert, OCI_ASSOC);
+        if (strtolower($table_name) == 'humans'){
+            $new_id = max($ert["ID"]) + 1;
+        } else if (strtolower($table_name) == 'orgs') {
+            $new_id = max($ert["ID"]) + 10;
+        }
+    } else {
+        echo "Not found";
     }
     return $new_id;
 }
 
 function add_pers($user, $password, $name, $surname, $id_of_org)
 {
+    $new_id = inc_id('Humans', $user, $password);
     $connection = oci_connect($user, $password, 'localhost/XEPDB1');
     if ($connection) {
-        $new_id = inc_id($connection, 'Human');
         $text = "INSERT INTO HUMANS (ID, NAME, SURNAME, ID_OF_ORG) values ($new_id, $name, $surname, $id_of_org)";
         $stid = oci_parse($connection, $text);
         oci_execute($stid);
@@ -60,7 +66,7 @@ function delete_org($user, $password)
 {
     $connection = oci_connect($user, $password, 'localhost/XEPDB1');
     if ($connection) {
-        
+
     } else {
         echo "Not found";
     }
@@ -84,3 +90,5 @@ function modify_org()
 {
 
 }
+
+add_pers('TEST', 'TEST','Nadya', 'Johns', 120);
